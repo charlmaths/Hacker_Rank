372fdcaa01a51f55ce158055e85dd646404a873d
charlmaths <122214610+charlmaths@users.noreply.github.com>
2023-08-02 16:05:13 +0100

    Added a new function that squares a number

---
+++ SquaredNumbers.py
@@ -0,0 +1,9 @@
+""" Simple code that squares an input integer """
+
+def squared_num(x):
+    """ Simple function that squares a number """
+    funcx = x ** 2
+    return funcx
+
+result = squared_num(5)
+print(result)
