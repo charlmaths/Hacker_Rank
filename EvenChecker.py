2e2875e6f550785fb1c4f959a487039ea4961592
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-08-31 13:19:35 +0100

    Added a new function to check if number is even

---
+++ EvenChecker.py
@@ -0,0 +1,10 @@
+# Simple function to check if number is even:
+
+def is_even(x):
+    "Using mod division - check the input number if == 0"
+    if x % 2 == 0:
+        print("Number is Even")
+    else:
+        print("Number is Odd")
+
+is_even(5)
